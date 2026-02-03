3b91eda79cf19930f230629506ee9f752f4a8a87
ThotDestroyerXXX <151725216+ThotDestroyerXXX@users.noreply.github.com>
2026-02-03 20:56:34 +0700

    first commit

---
+++ src/test/java/com/example/taxone/TaxoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
